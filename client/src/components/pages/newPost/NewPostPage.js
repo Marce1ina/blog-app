1cafd5307b8ae92810101b1658a28cb059f7a523
Marcelina Kucięba <mkucieba@egnyte.com>
2019-08-19 20:54:02 +0200

    Revert "Base structure + routing"
    This reverts commit af68208caf926ad22d45b357a33e4fab09fa4a46.

--- client/src/components/pages/newPost/NewPostPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NewPostPage = () => (
-    <div>
-        <h1>Add new post</h1>
-    </div>
-);
-
-export default NewPostPage;
--- client/src/components/pages/singlePost/SinglePostPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SinglePostPage = () => (
-    <div>
-        <h1>Single Post</h1>
-    </div>
-);
-
-export default SinglePostPage;
